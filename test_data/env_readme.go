efb20f7c773b07400c69690e73b11cc34e937173
Yuzuki616 <yuzuki2332@gmail.com>
2025-03-30 07:43:06 +0900

    feat: example env file

---
+++ test_data/env_readme.go
@@ -0,0 +1,11 @@
+package test_data
+
+// Copy this file to env.go and uncomment the following code then fill in the values.
+/*
+const (
+	PanelUrl      = ""
+	PanelNodeId   = 0
+	PanelNodeType = ""
+	PanelKey      = ""
+)
+*/
